9e4737c1ef7837bb993116cef3ee5523daf8fa93
YazidJanati <36244825+YazidJanati@users.noreply.github.com>
2018-03-15 23:34:18 +0100

    Update Restaurants.java

--- Restaurants.java
+++ Restaurants.java
@@ -1,8 +1,22 @@
 package restaurants;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import Localisation.Grid;
 
 
-public class Restaurants<K,V> {
-	ArrayList<K> liste=new ArrayList<K>();	
+public class Restaurants {
+	public HashMap<Grid,List<Restaurant>> liste=new HashMap<Grid,List<Restaurant>>();	
+	private void putvalue(Map<Grid,List<Restaurant>> Restos,Grid key,Restaurant value) {
+		List<Restaurant> restos=Restos.get(key);
+		if (restos ==null) {
+			restos=new ArrayList<Restaurant>();
+			Restos.put(key,restos);
+		}
+		restos.add(value);		
+	}
+	
 	
 	}
