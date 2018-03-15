c4da2b59057d1b3752090bece2e1c85d1addacc7
YazidJanati <36244825+YazidJanati@users.noreply.github.com>
2018-03-16 00:04:10 +0100

    Update Locate.java

--- Locate.java
+++ Locate.java
@@ -9,14 +9,6 @@ import java.util.List;
 
 
 public class Locate {
-	private void putvalue(Map<Grid,List<Restaurant>> Restos,Grid key,Restaurant value) {
-		List<Restaurant> restos=Restos.get(key);
-		if (restos ==null) {
-			restos=new ArrayList<Restaurant>();
-			Restos.put(key,restos);
-		}
-		restos.add(value);		
-	}
 	public static double distance(Client client, Restaurant resto) {
 		return 6378 * (Math.PI/2 - Math.asin(Math.sin((Math.PI * client.latitude) / 180) * Math.sin(Math.PI * resto.latitude) / 180) + Math.cos((Math.PI * client.longitude/180) - (Math.PI * resto.longitude/180)) * Math.cos(Math.PI * resto.latitude / 180) * Math.cos(Math.PI * client.latitude/180));
 	}
