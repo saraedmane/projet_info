84013de8225bd8ab6e28e364a0ae815254441b07
YazidJanati <36244825+YazidJanati@users.noreply.github.com>
2018-03-21 00:40:49 +0100

    Add files via upload

--- Grid.java
+++ Grid.java
@@ -1,11 +1,46 @@
 package Localisation;
 import restaurants.Client;
+import java.util.List;
+import java.util.ArrayList;
+import restaurants.Restaurant;
 
 public class Grid {
-	String grid;
-	public static Grid whichGrid(Client client) {
-		return null;
+	public List<Case> boxes;
+	public Grid() {
+		boxes=new ArrayList<Case>();
 	}
-	
-
+	public void makeBoxes() {
+		Coordinates left=new Coordinates(0,0);
+		Coordinates right=new Coordinates(0,0);
+		Coordinates upleft=new Coordinates(0,0);
+		Coordinates upright=new Coordinates(0,0);
+		for (int i=-90;i<90;i++) {
+			for (int j=-180;j<180;j++) {
+				left.latitude=i;
+				left.longitude=j;
+				right.latitude=i;
+				right.longitude=j+1;
+				upleft.latitude=i+1;
+				upleft.longitude=j;
+				upright.latitude=i+1;
+				upright.longitude=j+1;
+				boxes.add(new Case(left,right,upleft,upright));
+			}
+		}
+		
+	}
+	public void addRestaurant(Restaurant resto) {
+		this.boxes.get((int) ((90+Math.floor(resto.latitude))*360 + 180 + Math.floor(resto.longitude))).liste.add(resto);
+	}
+	public ArrayList<Restaurant> listof(Client client,double distance) {
+		ArrayList<Restaurant> result=new ArrayList<Restaurant>();
+		List<Restaurant> relevant=this.boxes.get((int) ((90+Math.floor(client.latitude))*360 + 180 + Math.floor(client.longitude))).liste;
+		for (Restaurant r: relevant) {
+			if (Locate.distance(client,r)<distance) {
+				result.add(r);			
+			}
+		}
+		return result;
+		
+	}
 }
