968d000833ff5853397f9876bd33262e2b5ced05
YazidJanati <36244825+YazidJanati@users.noreply.github.com>
2018-03-21 00:42:47 +0100

    Add files via upload

---
+++ Coordinates.java
@@ -0,0 +1,11 @@
+package Localisation;
+
+public class Coordinates {
+	public double latitude;
+	double longitude;
+	public Coordinates(double lat, double longi) {
+		latitude=lat;
+		longitude=longi;		
+	}
+
+}
