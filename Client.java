f62a046057872f1e29f45f2b0bb97c4f37883646
YazidJanati <36244825+YazidJanati@users.noreply.github.com>
2018-04-16 21:39:02 +0200

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,12 @@
+package restaurants;
+
+public class Client {
+	public double longitude;
+	public double latitude;
+	public Spécialité spécialité;
+	public double distance;
+	public Client(double latitude,double longitude) {
+		this.longitude=longitude;
+		this.latitude=latitude;	
+	}
+}
