a7c2c1a937a8c06305953f7b1798e64e0c0c3517
saraedmane <edmane.sara@gmail.com>
2018-04-25 18:51:41 +0200

    app android

---
+++ android/app/src/main/java/com/edmane/projetinfo/Client.java
@@ -0,0 +1,17 @@
+package com.edmane.projetinfo;
+
+/**
+ * Created by user on 02/04/2018.
+ */
+
+public class Client {
+    public double longitude;
+    public double latitude;
+    public Spécialité spécialité;
+    public double distance;
+    public Client(double latitude,double longitude) {
+        this.longitude=longitude;
+        this.latitude=latitude;
+    }
+    }
+
